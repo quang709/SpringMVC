3e7c02d39b398764faca0444a65d282ebadedbc6
quang709 <quangquangquang321@gmail.com>
2021-01-22 15:26:42 +0700

    quan he 1- n (Room - RoomCategory) new

--- src/main/java/com/codegym/repository/EthnicityRepository.java
+++
@@ -1,7 +0,0 @@
-package com.codegym.repository;
-
-import com.codegym.model.Ethnicity;
-import org.springframework.data.repository.PagingAndSortingRepository;
-
-public interface EthnicityRepository extends PagingAndSortingRepository<Ethnicity,Long> {
-}
--- src/main/java/com/codegym/repository/RoomCategoryRepository.java
+++
@@ -1,7 +0,0 @@
-package com.codegym.repository;
-
-import com.codegym.model.RoomCategory;
-import org.springframework.data.repository.PagingAndSortingRepository;
-
-public interface RoomCategoryRepository extends PagingAndSortingRepository<RoomCategory,Long> {
-}
